5298776e7b29ced48f0104474db6148c9beea890
Kung Lo <kungyang.lo@revature.com>
2024-10-02 15:59:01 -0400

    Added new project

--- P0/Program.cs
+++ P0/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        User me = new("Kung");
+        
+        Console.WriteLine(me.Firstname);
     }
 }
---
+++ P0/User.cs
@@ -0,0 +1,11 @@
+namespace P0;
+
+class User
+{
+    public string Firstname { get; set; }
+
+    public User(string firstname)
+    {
+        this.Firstname = firstname;
+    }
+}
